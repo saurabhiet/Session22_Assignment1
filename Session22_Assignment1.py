d56cae4083a356d095094c4e457ac627852981b8
saurabhiet <35842602+saurabhiet@users.noreply.github.com>
2018-06-12 18:19:12 +0530

    Add files via upload

---
+++ Session22_Assignment1.py
@@ -0,0 +1,27 @@
+﻿import numpy as np
+from sklearn import cluster, datasets
+from scipy import misc
+import matplotlib.pyplot as plt
+%matplotlib inline
+face = misc.face(gray=True)
+
+n_clusters = 5
+np.random.seed(0)
+
+X = face.reshape((-1, 1)) 
+k_means = cluster.KMeans(n_clusters=n_clusters, n_init=4)
+k_means.fit(X)
+values = k_means.cluster_centers_.squeeze()
+labels = k_means.labels_
+
+face_compressed = np.choose(labels, values)
+face_compressed.shape = face.shape
+
+vmin = face.min()
+vmax = face.max()
+# original face
+plt.figure(1, figsize=(3, 2.2))
+plt.imshow(face, cmap=plt.cm.gray, vmin=vmin, vmax=256)
+# compressed face
+plt.figure(2, figsize=(3, 2.2))
+plt.imshow(face_compressed, cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
